95387f98ec085b8f9d9cae540b0c5368672ba87d
Caio Miranda <caiomiranda2000.cm@gmail.com>
2024-06-03 03:17:53 -0300

    Adjust config.yml

--- app/deploy_app.sh
+++ app/deploy_app.sh
@@ -11,4 +11,5 @@ terraform plan
 terraform apply -auto-approve \
   -var "private_key_path=$HOME/.ssh/id_rsa" \
   -var "aws_access_key=$AWS_ACCESS_KEY_ID" \
-  -var "aws_secret_key=$AWS_SECRET_ACCESS_KEY"
+  -var "aws_secret_key=$AWS_SECRET_ACCESS_KEY" \
+  -var "key_name=$AWS_KEY_NAME"
