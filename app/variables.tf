b85a0dcc9a8220fce6652bd56dba1e193951cc31
Caio Miranda <caiomiranda2000.cm@gmail.com>
2024-06-06 22:51:49 -0300

    Adjust Terraform files

--- app/variables.tf
+++ app/variables.tf
@@ -13,13 +13,13 @@ variable "instance_type" {
   default     = "t2.micro"
 }
 
-# variable "aws_access_key" {
-#   description = "AWS Access Key"
-# }
+variable "aws_access_key" {
+  description = "AWS Access Key"
+}
 
-# variable "aws_secret_key" {
-#   description = "AWS Secret Key"
-# }
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+}
 
 # variable "key_name" {
 #   description = "SSH Key"
